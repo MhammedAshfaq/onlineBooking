03fb82c923041aa2e827e9929383ba48a760edc3
Ashaaa <apple@Apples-MacBook-Pro.local>
2024-07-09 23:56:57 +0530

    Updated server url

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://technotes-api.onrender.com/api";
+export const BASE_URL = "https://technotes-api-ibj7.onrender.com/api";
 
 export const VITE_CLOUD_NAME = "delaptfsr";
 export const VITE_UPLOAD_PRESET = "doctor_booking_system";
